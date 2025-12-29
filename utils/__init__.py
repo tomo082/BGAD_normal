eecda1656ffe3d00fbf0410c67dd21f1721d3aba
tomo082 <131239927+tomo082@users.noreply.github.com>
2025-12-30 01:54:47 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .model_utils import save_results, save_weights, load_weights, adjust_learning_rate, warmup_learning_rate
+from .model_utils import save_results, save_weights, load_weights, adjust_learning_rate, warmup_learning_rate,save_weights_ada, load_weights_ada
 from .visualizer import denormalization, export_groundtruth, export_hist, export_scores, export_test_images, plot_visualizing_results
 from .utils import MetricRecorder, EachEpochRecorder, get_logp, t2np, rescale, calculate_pro_metric, evaluate_thresholds, convert_to_anomaly_scores
 
@@ -6,6 +6,8 @@ from .utils import MetricRecorder, EachEpochRecorder, get_logp, t2np, rescale, c
 __all__ = ['save_results',
            'save_weights',
            'load_weights',
+           'sava_weights_ada',
+           'load_weights_ada',
            'adjust_learning_rate',
            'warmup_learning_rate',
            'denormalization',
