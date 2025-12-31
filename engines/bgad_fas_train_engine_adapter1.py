812587229adaf32ca15aacde4d8679f386fd465e
tomo082 <131239927+tomo082@users.noreply.github.com>
2025-12-31 16:52:00 +0900

    Update bgad_fas_train_engine_adapter1.py

--- engines/bgad_fas_train_engine_adapter1.py
+++ engines/bgad_fas_train_engine_adapter1.py
@@ -84,6 +84,7 @@ def train_meta_epoch(args, epoch, data_loader, encoder, decoders, optimizer,opti
 
                             optimizer.zero_grad()
                             loss.backward()
+                            torch.nn.utils.clip_grad_norm_(adapters.parameters(), max_norm=1.0)
                             optimizer.step()
                             optimizer_ada.step()
                             total_loss += loss.item()
@@ -128,6 +129,7 @@ def train_meta_epoch(args, epoch, data_loader, encoder, decoders, optimizer,opti
                         optimizer.zero_grad()
                         optimizer_ada.zero_grad()
                         loss.backward()
+                        torch.nn.utils.clip_grad_norm_(adapters.parameters(), max_norm=1.0)
                         optimizer.step()
                         optimizer_ada.step()
                         loss_item = loss.item()
